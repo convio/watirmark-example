3f06ba6e44f348e120709b977a073470db3a1a77
Eric Slater <eric9970@sbcglobal.net>
2012-03-19 21:50:24 -0500

    removed references to contact. stripped conttrollers out of file and class name.

--- lib/salesforce.rb
+++ lib/salesforce.rb
@@ -20,8 +20,5 @@ require 'salesforce/components/lead/lead'
 require 'salesforce/components/lead/lead_view'
 require 'salesforce/components/lead/lead_model'
 
-require 'salesforce/components/contact/contact_model'
-require 'salesforce/components/contact/contact_view'
-require 'salesforce/components/contact/contact'
 
 
